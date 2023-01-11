acfaddb5b4461e81833f8dc9a7dcf5ac30dcb603
JairzinhoTorres <jair1924torres@gmail.com>
2023-01-11 21:30:15 +0000

    Esta casi pronto, queda hacer que cada li se meta en una nueva card y que no esten todas las tareas dentro de la misma card

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,56 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+
 const Home = () => {
+	const[tarea,setTarea]=useState("")	
+	const[lista,setLista]=useState([])
+// 	const listaItems = lista.map((item) =>
+// 	<li  key={item.toString()}>{item}
+// 	<button type="button" className="btn btn-primary" id={item}>X</button>
+//   </li>
+// );
+// ------------------------------Para borrar datos----------------------------------------------------
+const DeleteItems = (indexItem) => {
+	setLista((prevState) =>
+	  prevState.filter((listaItems, index) => index !== indexItem)
+	);
+  };
+// ------------------------------Para enviar datos----------------------------------------------------
+	function enviarDatos(e) {
+		e.preventDefault()
+		setLista([...lista, tarea])
+		setTarea("")
+	}
+	console.log(lista);
+	
+// ----------------------------------------------------------------------------------
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<>
+		
+		
+		<div className="card container d-flex mt-3">
+  <div className="card-body">
+	<input  type="text" className="input m-1 w-75" value={tarea} aria-describedby="este es el input del boton"  onChange={(e)=>{setTarea(e.target.value)}} placeholder="No hay tareas, añadir tareas ..."/>
+	<button type="submit" className="btn btn-primary btn-sm" onChange={(e)=>{setLista(e.target.value)}} onClick={enviarDatos}>Agregar tarea</button>
+  </div>
+
+  <div className="to-do-list d-flex card border-primary mb-3" style={{width: "18rem;"}} >
+  <div class="card-body text-primary">
+
+			  <ul>{lista.map((item, index) => (
+        <li key={index}>
+          {item}
+          <button className="btn" onClick={() => DeleteItems(index)}>
+            <i className="fas fa-trash-alt" />
+          </button>
+        </li>
+      ))}</ul>
+			</div>
 		</div>
+		</div>
+		
+		</>
 	);
 };
 
